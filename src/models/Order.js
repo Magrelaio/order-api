ce9b7c51f86ec9d3cf4cea013063d0d910782006
Magrelaio <caiocesar2004gfgff@gmail.com>
2026-03-08 14:39:09 -0300

    feat: inicializando o projeto com package.json e model basico para order.

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,22 +1,35 @@
 const mongoose = require("mongoose");
 
 const OrderSchema = new mongoose.Schema({
-  customerName: {
+  orderId: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
-  product: {
-    type: String,
-    required: true
-  },
-  quantity: {
+  value: {
     type: Number,
     required: true
   },
-  status: {
-    type: String,
-    default: "pending"
-  }
+  creationDate: {
+    type: Date,
+    required: true
+  },
+  items: [{
+    productId: {
+      type: Number,
+      required: true
+    },
+    quantity: {
+      type: Number,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    }
+  }]
+}, {
+  timestamps: true
 });
 
 module.exports = mongoose.model("Order", OrderSchema);
